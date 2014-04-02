8c1b279525ddb6a2b12eca0e5c31a6717efbee65
João Henriques <joaoh88@gmail.com>
2014-04-02 17:19:34 +0100

    Some changes on the server interface

--- TDIN-chatlib/ChatSeverInterface.cs
+++ TDIN-chatlib/ChatSeverInterface.cs
@@ -6,15 +6,21 @@ using System.Threading.Tasks;
 
 namespace TDIN_chatlib
 {
-    interface ChatSeverInterface
+    public interface ChatSeverInterface
     {
         IList<IPAddress> getActiveClients();
 
-        // Chamado pelo cliente quando se pretende registar no server
-        // deve retornar uma hash aleatória única
-        string register(IPAddress address);
+        /// <summary>Chamado pelo cliente quando se pretende registar no server.
+        /// É fornecido o username e password do cliente, caso não esteja registado o servidor deve registar um novo utilizador.
+        /// </summary>
+        /// <exception cref="ChatException">Em caso de não ser possível registar novo user ou password estiver errada</exception>
+        /// <returns>Em caso de sucesso a hash de sessão, em caso de erro null</returns>
+        string registerClient(IPAddress address, string user, string pass);
 
-        // Chamado pelo cliente quando se pretende desconectar do server
-        void disconnect(string hashCode);
+
+        /// <summary>Chamado pelo cliente quando se pretende desconectar do server</summary>
+        /// <param name="hashCode">hash retornado pelo server em registerClient</param>
+        void disconnectClient(string hashCode);
+
     }
 }
