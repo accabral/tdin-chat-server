34d280f5e734f148bb1e5aac6d8e4b7b540032ad
João Henriques <joaoh88@gmail.com>
2014-04-03 18:13:22 +0100

    Fixed problem with registering service and .NET security

--- TDIN-chatserver/ChatServer.cs
+++ TDIN-chatserver/ChatServer.cs
@@ -9,64 +9,20 @@ using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 
 namespace TDIN_chatserver
 {
-    class ChatServer : TDIN_chatlib.ChatSeverInterface
+    class ChatServer : MarshalByRefObject, TDIN_chatlib.ChatSeverInterface
     {
 
-        private IList<TDIN_chatlib.IPUser> activeClients;
+        private IList<TDIN_chatlib.IPUser> activeClients = new List<TDIN_chatlib.IPUser>();
         // Redudant... The string is the same as inside de UserSession class. 
         // The users are also in the activeclients IList.
         private Dictionary<string, TDIN_chatlib.UserSession> sessions = new Dictionary<string,TDIN_chatlib.UserSession>();
 
         public ChatServer()
         {
-            registerServer();
-        }
-
-        /// <summary>
-        /// Starts the server and registers it.
-        /// </summary>
-        private static void registerServer()
-        {
-            IDictionary props = new Hashtable();
-            props["port"] = 0;
-
-            BinaryServerFormatterSinkProvider serverProvider = new BinaryServerFormatterSinkProvider();
-            serverProvider.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
-            BinaryClientFormatterSinkProvider clientProvider = new BinaryClientFormatterSinkProvider();
-            TcpChannel chan = new TcpChannel(props, clientProvider, serverProvider);  // instantiate the channel
-            ChannelServices.RegisterChannel(chan, false);                             // register the channel
-
-            ChannelDataStore data = (ChannelDataStore)chan.ChannelData;
-
-            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
-            string _ip = "127.0.0.1";
-
-
-            foreach (IPAddress ip in IPHost.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    _ip = ip.ToString();
-                    // yep, dont stop and grab the last one on the list
-                }
-            }
-
-            TDIN_chatlib.IPAddress localAddress = new TDIN_chatlib.IPAddress(
-                                _ip, // get IP
-                                new Uri(data.ChannelUris[0]).Port  // get the port
-                            );
-
-            Console.WriteLine("a: " + localAddress.IP + ", p: " + localAddress.PORT);
-
-            Console.WriteLine("* Registering Server Object");
-            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Program),
-                           TDIN_chatlib.Constants.SERVER_SERVICE,
-                           WellKnownObjectMode.Singleton);
-
-            System.Console.ReadLine();
         }
 
         /// <summary>
@@ -78,6 +34,32 @@ namespace TDIN_chatserver
             return this.activeClients;
         }
 
+
+        private string generateRandomHash()
+        {
+            byte[] bytes = new byte[16],
+                   byteOut;
+            string hash = String.Empty;
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            using (var crypt = new SHA256Managed())
+            {
+                byteOut = crypt.ComputeHash(bytes);
+            }
+
+            foreach (byte bit in byteOut)
+            {
+                hash += bit.ToString("x2");
+            }
+
+            return hash;
+        }
+
+
         /// <summary>
         /// 
         /// </summary>
@@ -88,11 +70,14 @@ namespace TDIN_chatserver
         {
             //TODO: Complete according with the interface specification.
             // Create user and add it to active users.
-            TDIN_chatlib.IPUser new_user = new TDIN_chatlib.IPUser(user.Username, user.Name);
+            TDIN_chatlib.IPUser new_user = new TDIN_chatlib.IPUser(user.Username, user.Name, address);
             activeClients.Add(new_user);
             // Build a session and return it.
-            TDIN_chatlib.UserSession session = new TDIN_chatlib.UserSession(user.Username, user.Name);
+            TDIN_chatlib.UserSession session = new TDIN_chatlib.UserSession(user.Username, user.Name, generateRandomHash());
             sessions.Add(session.SessionHash, session);
+
+            Console.WriteLine("* [" + session.SessionHash + "] New user: " + session.Username);
+
             return session;
         }
 
--- TDIN-chatserver/Program.cs
+++ TDIN-chatserver/Program.cs
@@ -9,16 +9,48 @@ using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters;
 
 
 namespace TDIN_chatserver
 {
     class Program
     {
+
+
+        private static void registerServer()
+        {
+
+            //Creating a custom formatter for a TcpChannel sink chain. 
+            BinaryServerFormatterSinkProvider provider = new BinaryServerFormatterSinkProvider();
+            provider.TypeFilterLevel = TypeFilterLevel.Full;
+            //Creating the IDictionary to set the port on the channel instance. 
+            IDictionary props = new Hashtable();
+            props["port"] = TDIN_chatlib.Constants.DEFAULT_SERVER_PORT;
+            //Pass the properties for the port setting and the server provider in the server chain argument. (Client remains null here.) 
+            TcpChannel chan = new TcpChannel(props, null, provider);
+            // register the channel
+            ChannelServices.RegisterChannel(chan, false);
+
+
+            //Console.WriteLine("a: " + localAddress.IP + ", p: " + localAddress.PORT);
+
+            Console.WriteLine("* Registering Server Object.");
+            Console.WriteLine("* Running on port: " + TDIN_chatlib.Constants.DEFAULT_SERVER_PORT);
+            
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(ChatServer),
+                                                                   TDIN_chatlib.Constants.SERVER_SERVICE,
+                                                                   WellKnownObjectMode.Singleton);
+
+            System.Console.ReadLine();
+        }
+
+
         static void Main(string[] args)
         {
-            new ChatServer();
-            
+            //new ChatServer();
+            registerServer();
         }
 
         
