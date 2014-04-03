4e3a5199c97f2a0cc46ced3e2e94d9e910e1216f
João Gonçalves <jsvgoncalves@gmail.com>
2014-04-03 13:42:17 +0100

    Basic program structure. Registers server and implements interface.

---
+++ TDIN-chatserver/ChatServer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TDIN_chatserver
+{
+    class ChatServer : TDIN_chatlib.ChatSeverInterface
+    {
+        public ChatServer()
+        {
+            registerServer();
+        }
+
+        /// <summary>
+        /// Starts the server and registers it.
+        /// </summary>
+        private static void registerServer()
+        {
+            IDictionary props = new Hashtable();
+            props["port"] = 0;
+
+            BinaryServerFormatterSinkProvider serverProvider = new BinaryServerFormatterSinkProvider();
+            serverProvider.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            BinaryClientFormatterSinkProvider clientProvider = new BinaryClientFormatterSinkProvider();
+            TcpChannel chan = new TcpChannel(props, clientProvider, serverProvider);  // instantiate the channel
+            ChannelServices.RegisterChannel(chan, false);                             // register the channel
+
+            ChannelDataStore data = (ChannelDataStore)chan.ChannelData;
+
+            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+            string _ip = "127.0.0.1";
+
+
+            foreach (IPAddress ip in IPHost.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    _ip = ip.ToString();
+                    // yep, dont stop and grab the last one on the list
+                }
+            }
+
+            TDIN_chatlib.IPAddress localAddress = new TDIN_chatlib.IPAddress(
+                                _ip, // get IP
+                                new Uri(data.ChannelUris[0]).Port  // get the port
+                            );
+
+            Console.WriteLine("a: " + localAddress.IP + ", p: " + localAddress.PORT);
+
+            Console.WriteLine("* Registering Server Object");
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Program),
+                           TDIN_chatlib.Constants.SERVER_SERVICE,
+                           WellKnownObjectMode.Singleton);
+
+            System.Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<TDIN_chatlib.IPUser> getActiveClients()
+        {
+
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public TDIN_chatlib.UserSession registerClient(TDIN_chatlib.IPAddress address, TDIN_chatlib.LoginUser user)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public TDIN_chatlib.User queryUser(string username)
+        {
+            System.Console.WriteLine("buh");
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hashCode"></param>
+        public void disconnectClient(string hashCode)
+        {
+            System.Console.WriteLine("buh");
+        }
+
+
+    }
+}
--- TDIN-chatserver/Program.cs
+++ TDIN-chatserver/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace TDIN_chatserver
 {
@@ -10,6 +17,10 @@ namespace TDIN_chatserver
     {
         static void Main(string[] args)
         {
+            new ChatServer();
+            
         }
+
+        
     }
 }
